94a48ab3cd679a40afdf1bede736fbac3d30d78c
Tasiddiqul Haque <md.tasiddiqul.haque@hp.com>
2017-06-14 15:29:50 -0500

    Added wrong info

--- sonarqube-scanner/src/java/HelloWorld.java
+++ sonarqube-scanner/src/java/HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World !");
+		noWay();
     }
+	
+	public static int noWay(){
+		
+		return "ABC";
+	}
 }
